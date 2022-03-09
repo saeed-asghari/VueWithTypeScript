87f00515488d2aeb2df974c1c1677c1d6c5dff15
Saeed Asghari <s.asghari@siteb.local>
2022-03-09 11:45:51 +0330

    change description home page

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,15 +1,18 @@
 <template>
-<div class="container mt-5">
+  <div class="container mt-5">
     <div class="row">
-        <div class="col-md-6">
-            <p>Navbar navigation links build on our .nav options with their own modifier class and require the use of toggler classes for proper responsive styling. Navigation in navbars will also grow to occupy as much horizontal space as possible to keep your navbar contents securely aligned.
-
-Add the .active class on .nav-link to indicate the current page.
-
-Please note that you should also add the aria-current attribute on the active .nav-link. </p>
-        </div>
+      <div class="col-md-12">
+        <pre>
+          I learned vue with typescript and created a project. access my project via 
+          GitHub link:
+          https://github.com/saeed-asghari/VueWithTypeScript 
+          my resource:
+          site:webprog.io (persian)
+          book: Large Scale Apps with Vue 3 and TypeScript(English)
+        </pre>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
